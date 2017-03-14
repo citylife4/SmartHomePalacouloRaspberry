882580014537890530da850527d5a9ce316424fe
José Valverde <delfimvalverde@gmail.com>
2017-03-14 23:07:27 +0000

    exception handler

--- app/views.py
+++ app/views.py
@@ -1,3 +1,5 @@
+import sys
+
 from app import app, login_manager, db
 from .models import User
 from forms import LoginForm
@@ -13,9 +15,12 @@ from gpio_func import get_distance_boolean, trigger_door
 @app.route('/')
 @login_required
 def index():
-
     if FOR_RASP:
-        distance, open_boolean = get_distance_boolean()
+        print("ola")
+        try:
+            distance, open_boolean = get_distance_boolean()
+        except:
+            print(sys.exc_info()[0])
 
         if (distance >= 300) or (distance <= 2):
             flash('Atualiza a pagina por favor')
@@ -40,7 +45,7 @@ def changeportao():
 
     distance, openboolean = get_distance_boolean()
 
-    if openboolean: # is closed
+    if openboolean:  # is closed
         flash('A fechar')
     else:  # is open
         flash('A abrir')
@@ -50,7 +55,9 @@ def changeportao():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    print 'ola'
     form = LoginForm()
+    print 'oi'
     if request.method == 'GET':
         return render_template('LoginForm.html', form=form)
 
@@ -63,12 +70,15 @@ def login():
     registered_user = User.query.filter_by(username=username).first()
     print(registered_user)
     if registered_user is None:
+        print 'username invalid'
         flash('Username is invalid', 'error')
         return redirect(url_for('login'))
     if not registered_user.check_password(password):
+        print 'password invalid'
         flash('Password is invalid', 'error')
         return redirect(url_for('login'))
     login_user(registered_user)
+    print("Valid")
     flash('Logged in successfully')
     return redirect(request.args.get('next') or url_for('index'))
 
